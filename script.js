424689db8272c823c1b6ba53beb2d69d5920ae22
thehouseoftheway <thehouseofthewayinnconventionc@gmail.com>
2024-10-22 23:04:52 +0800

    script.js

--- script.js
+++ script.js
@@ -26,6 +26,23 @@ function toggleLogin() {
     loginForm.classList.toggle('show'); // Add/Remove show class 
   }  
 
+// This part is modal function
+var modal = document.getElementById("myModal");
+var images = document.querySelectorAll("img[id^='myImg']");
+
+images.forEach(function (img) {
+    img.onclick = function () {
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+    }
+});
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function () {
+    modal.style.display = "none";
+}
+// end part is modal function 
+
 
 
   
